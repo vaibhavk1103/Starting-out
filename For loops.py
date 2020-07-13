8e673ec4bb05b6a1e75013f21d14b79036803a2d
Vaibhav Kushwaha <68052520+vaibhavk1103@users.noreply.github.com>
2020-07-13 12:06:51 +0530

    Add files via upload

--- For loops.py
+++ For loops.py
@@ -1,5 +1,6 @@
 # Write a program to count till a given number and print out the numbers.
 
-x = int(input("Enter the number upto which you want the numbers to appear:"))
-for a in range(0,x + 1):
+x = int(input("Enter the number from which you want to start:"))
+y = int(input("Enter the number upto which you want the numbers to appear:"))
+for a in range(x, y + 1):
     print(a)
